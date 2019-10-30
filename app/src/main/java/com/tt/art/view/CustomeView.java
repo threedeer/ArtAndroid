ada63db0e940326f96a9532800375454a5944a4a
threedeer <1224179883@qq.com>
2019-10-30 21:17:33 +0800

    测试自定义内容

---
+++ app/src/main/java/com/tt/art/view/CustomeView.java
@@ -0,0 +1,24 @@
+package com.tt.art.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @author T
+ * @date 2019/10/30
+ */
+public class CustomeView extends View {
+    public CustomeView(Context context) {
+        super(context);
+    }
+
+    public CustomeView(Context context,  @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomeView(Context context,  @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
